49e8992529c8db69415e212bc574dea5210e7bcb
megalith <megalith@root.sh>
2023-01-23 10:52:59 -0800

    more work on the message base subsystem

--- messages/main.go
+++ messages/main.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"fmt"
 	"net"
+	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
@@ -44,23 +46,73 @@ func messageEditor(conn net.Conn, db *sql.DB) error {
 	return nil
 }
 
-func scanForNewMessages(db *sql.DB, lastRead time.Time) ([]Message, error) {
+func scanForNewMessages(db *sql.DB, lastRead time.Time) ([]util.Message, error) {
 	rows, err := db.Query("SELECT id, basename, subject, author, date, message, postto FROM messages WHERE date > ?", lastRead)
 	if err != nil {
-		return nil, fmt.Errorf("Error querying messages: %v", err)
+		return nil, fmt.Errorf("error querying messages: %v", err)
 	}
 	defer rows.Close()
 
-	var messages util.[]Message
+	var messages []util.Message
 	for rows.Next() {
-		var m Message
+		var m util.Message
 		if err := rows.Scan(&m.ID, &m.Basename, &m.Subject, &m.Author, &m.Date, &m.Message, &m.Postto); err != nil {
-			return nil, fmt.Errorf("Error scanning message: %v", err)
+			return nil, fmt.Errorf("error scanning message: %v", err)
 		}
 		messages = append(messages, m)
 	}
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("Error fetching messages: %v", err)
+		return nil, fmt.Errorf("error fetching messages: %v", err)
 	}
 	return messages, nil
+}
+func fullScreenEditor(conn net.Conn, header, messageBase string) string {
+	var input string
+	conn.Write([]byte(header + "\n"))
+	conn.Write([]byte("Message Base: " + messageBase + "\n"))
+	conn.Write([]byte("Ctl-Q to Quit | Ctl-S to save and post\n"))
+
+	// get terminal size
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = conn
+	out, _ := cmd.Output()
+	size := strings.Split(string(out), " ")
+	rows, _ := strconv.Atoi(size[0])
+
+	reader := bufio.NewReader(conn)
+	for {
+		line, _, _ := reader.ReadLine()
+		input += string(line) + "\n"
+		conn.Write([]byte("\033[1A")) // move cursor up one line
+		conn.Write([]byte("\033[K"))  // clear current line
+		// unsure what this is, keeping to test:
+		//if len(input) > 0 && input[len(input)-2:] == ".\n" {
+		//	break
+		//}
+		if len(input) > 0 && input[0] == '.' {
+			switch input {
+			case ".s":
+				// Save and post message
+				if err := postMessage(db, "general", subject, author, postto, message); err != nil {
+					conn.Write([]byte("Error posting message: " + err.Error()))
+					return err
+				}
+				conn.Write([]byte("Message posted successfully."))
+				return nil
+			case ".q":
+				// Quit editor without saving
+				conn.Write([]byte("Exiting editor.\n"))
+				return nil
+			}
+		}
+		if len(strings.Split(input, "\n")) >= rows-2 { // subtract 2 to account for the header and bottom bar
+			conn.Write([]byte("\033[2J")) // clear screen
+			conn.Write([]byte("\033[H"))  // move cursor to top left
+			conn.Write([]byte(header + "\n"))
+			conn.Write([]byte("Message Base: " + messageBase + "\n"))
+			conn.Write([]byte("Ctl-Q to Quit | Ctl-S to save and post\n"))
+			conn.Write([]byte(input))
+		}
+	}
+	return input
 }
